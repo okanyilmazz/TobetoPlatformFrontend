f380b6726add281a7b9be6106a33eecfe97e4245
dianakaynak <dianaonder90@gmail.com>
2024-01-15 15:49:21 +0300

    Add an area on the AboutUs

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -4,7 +4,7 @@ import { Col, Container, Image, Row } from 'react-bootstrap'
 
 function AboutUs() {
     return (
-        <div className=' bg-front-dark  '  >
+        <div className=' bg-front-dark about-content'  >
             <Container >
                 <Row className='aboutUs'>
                     <Col md={2}>
@@ -28,8 +28,56 @@ function AboutUs() {
                         <video controls className='aboutUs-video' src="https://s3.cloud.ngn.com.tr/tobeto/tobeto_final_v2_5c7893fbe0.mp4"    ></video>
                     </Col>
                 </Row>
+                <Row className='container mt-5'>
+                    <Col md={12} >
+                        <p className='text-area text-white'>
+                            Geleceğin mesleklerindeki yetenek açığını, geleneksel "headhunting" yaklaşımının <b>"headfarming" </b>olarak değişmesiyle çözülebileceğine inanarak yola çıktık. <br /><br />
+
+                            Tobeto, "headfarming" yaklaşımıyla yeteneği potansiyel olarak değerlendirir, en kıymetli olacağı alanda yetiştirir, değer yaratacağı projeler ve kurumlarla eşleştirir. Bu fırsata <b>Y.E.S. (Yetiş-Eşleş-Sürdür) </b>diyen herkesi Tobeto’lu olmaya çağırıyoruz.<br /><br />
+
+                            Günümüzde iş bulmak, bir projeye dahil olmak veya kariyerinde yükselmek gibi değer yaratma yolları için en önemli unsurların başında <b>dijital beceri</b> sahibi olmak geliyor. Tam da bu ihtiyaçları kapsamak üzere, Tobeto'da eğitim içeriklerimizi hem dijital beceri sahibi olmak isteyen yeteneklerin teknik beceri kazanması hem de genç profesyonellerin, ihtiyaçlarına uygun olarak yenilenmeleri ve yetkinliklerini geliştirmelerini kapsayacak şekilde tek platformda birleştirdik.<br /><br />
+
+                            Tobeto’da <b>hem bireylere hem de kurumlara</b> hizmet eden yapımızla, doğru yetenekle doğru pozisyonun sürdürülebilir bir şekilde eşleşmesine ve birlikte gelişmelerine alan açıyoruz. Kurum içi çözümlere destek veriyor, eğitim ve istihdam arasında köprü görevini üstleniyoruz.
+
+                        </p>
+
+
+                    </Col>
+
+
+                </Row>
             </Container>
+
+            <div className='text-white'>
+                <div className='aboutUs-container'>
+                    <div className='container' >
+                        <div className="row">
+                            <div className="about-list">
+                                <ul>
+                                    <div className="list-item">
+                                        <h1 className='tobeto-header'>Tobeto farkı;</h1>
+                                        <li> <b>Codecademy </b> ile uluslararası geçerliliğe sahip sertifika fırsatı</li> <br />
+                                        <li> <b>Abonelik</b>modeliyle eğitime erişim</li><br />
+                                        <li>Çeşitlendirilmiş <b> değerlendirme </b> araçlarıyla gelişim alanlarını tanıma</li><br />
+                                        <li> <b> Fark yaratan </b>  bir gelişim süreci</li><br />
+                                        <li>Mesleki eğitimlerin yanı sıra <b>kişisel ve profesyonel gelişim</b> imkanı</li><br />
+                                        <li>Alanında uzman eğitmenlerle  <b> canlı dersler</b></li><br />
+                                        <li><b> Mentör </b>   desteği</li><br />
+                                        <li>Online ve canlı derslerle <b>hibrit</b> yaklaşım</li><br />
+                                        <li><b> Zengin </b>   eğitim kütüphanesi</li>
+                                    </div>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div >
+
         </div >
+
+
+
+
     )
 }
 

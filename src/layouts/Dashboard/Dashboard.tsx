a58c56cae48a8cc3d70d4816497ae5508c1a8d70
kubranuryavuz <nurkacm@gmail.com>
2024-01-18 18:12:17 +0300

    add forIndividuals

--- src/layouts/Dashboard/Dashboard.tsx
+++ src/layouts/Dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Homepage from '../../pages/Homepage/Homepage'
 import AboutUs from '../../pages/AboutUs/AboutUs'
+import EducationCard from '../../components/EducationCard/EducationCard'
 import ForInstitutionsPage from '../../pages/ForInstitutionsPage/ForInstitutionsPage'
 import { ForIndividuals } from '../../pages/ForIndividuals/ForIndividuals'
 import Calendar from '../../pages/Calendar/Calendar'

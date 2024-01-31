31af0eb704afac90bdf10a57a38b75bf614571ea
rumeysa22 <77965395+rumeysa22@users.noreply.github.com>
2024-01-31 20:44:39 +0300

    The Blog and WeInThePress pages have been added. (#55)
    * Add TrainingCard component
    
    * Add an area on the Homepage
    
    * The Blog and WeInThePress pages have been added.

---
+++ src/pages/BlogDetail/BlogDetail.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Paginate } from "../../models/paginate";
+import GetListBlogResponse from "../../models/responses/blog/getListBlogResponse";
+import blogService from "../../services/blogService";
+
+export default function BlogDetail() {
+    let { id } = useParams();
+
+    const [blogs, setBlogs] = useState<Paginate<GetListBlogResponse>>();
+
+    useEffect(() => {
+        blogService.getAll().then(result => {
+            setBlogs(result.data);
+        })
+    }, []);
+
+    return (
+        <div style={{ color: "blue", marginTop: "25rem" }}>
+            detay sayfası
+        </div>
+    )
+}

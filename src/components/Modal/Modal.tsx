bbb91314704716988fe26f0c53d7dca0c93898c4
anillgultekinn <anilgtekin@gmail.com>
2024-02-13 16:23:04 +0300

    merge

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import "./Modals.css";
+
+function Modals(props: any) {
+    const handleClose = () => {
+        props.onHide();
+    };
+
+    return (
+        <div>
+            <Modal className='modals-container' show={props.show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{props.examDetails}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body> {props.body}</Modal.Body>
+                <Modal.Footer>
+                    {props.footer}
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
+}
+
+export default Modals;
